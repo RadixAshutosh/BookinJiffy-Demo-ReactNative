a361292a4d6515359337fa47d56341f5816d5d6f
Radix Ashutosh <ashutoshw@tech-radix.com>
2022-05-13 12:39:31 +0530

    changes in AboutBIJ file

--- src/Screens/Sidebar/AboutBIJ.js
+++ src/Screens/Sidebar/AboutBIJ.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const AboutBIJ = () => {
   return (
-    <View>
+    <View style={styles.body}>
       <Text>AboutBIJ</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  body: {
+       flex: 1,
+       justifyContent: "center",
+       alignItems:"center"
+
+  }
+})
 
 export default AboutBIJ
